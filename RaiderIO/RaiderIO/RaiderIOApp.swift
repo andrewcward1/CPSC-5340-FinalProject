eec07bdf89a7a0080295af8c3b0ca6990bb0cede
Andrew <acw0151@auburn.edu>
2025-04-06 12:10:52 -0500

    Initial Commit 4.6.25

---
+++ RaiderIO/RaiderIO/RaiderIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaiderIOApp.swift
+//  RaiderIO
+//
+//  Created by Andrew Ward on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RaiderIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
