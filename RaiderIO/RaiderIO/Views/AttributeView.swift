3ae3dc7d090c3d6446c3d9ae7f5bdd244acdf8dd
Andrew <acw0151@auburn.edu>
2025-04-27 11:41:16 -0500

    Project Complete 4.27.25

---
+++ RaiderIO/RaiderIO/Views/AttributeView.swift
@@ -0,0 +1,18 @@
+//
+//  AttributeView.swift
+//  RaiderIO
+//
+//  Created by Andrew Ward on 4/27/25.
+//
+
+import SwiftUI
+
+struct AttributeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttributeView()
+}
---
+++ RaiderIO/RaiderIO/Views/CharacterDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterDetailView.swift
+//  RaiderIO
+//
+//  Created by Andrew Ward on 4/27/25.
+//
+
+import Foundation
